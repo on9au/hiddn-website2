1bd92d4acd2d4cb1787cca887b350cfdf934cd6a
null <151502370+nulluser0@users.noreply.github.com>
2024-08-28 12:32:55 +1000

    chore: Update Tailwind CSS configuration and add new color palette

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        albertsans: ['"Albert Sans"', "sans-serif"]
+      },
+      colors: {
+        'hiddn': {
+          '50': '#fefaec',
+          '100': '#fcf0c9',
+          '200': '#f8de8f',
+          '300': '#f5c754',
+          '400': '#f3b536',
+          '500': '#ec9114',
+          '600': '#d16c0e',
+          '700': '#ad4c10',
+          '800': '#8d3b13',
+          '900': '#743113',
+          '950': '#421806',
+        },
+      }
+    },
   },
   plugins: [],
 }
