acf0891dc10ee1ea7c969041df6a49700f804077
null <151502370+nulluser0@users.noreply.github.com>
2024-09-01 20:16:31 +1000

    refactor: Update user dashboard layout and add plan information
    The code changes update the layout of the user dashboard by adding a new section to display plan information. This includes the expiration date and status of the plan, as well as a placeholder for a data usage bar. These changes enhance the user experience and align with the established commit message conventions.

--- client/src/pages/user/dashboard.tsx
+++ client/src/pages/user/dashboard.tsx
@@ -14,9 +14,15 @@ const Dashboard: React.FC = () => {
 
     return (
         <div className="flex flex-col items-center min-h-screen pt-7">
-            <span className="w-full text-left">
-                <h1 className="text-4xl">{greeting}.</h1>
+            <span className="w-full text-left mb-6">
+                <h1 className="text-4xl font-semibold">{greeting}.</h1>
             </span>
+            <div className="w-full mb-6 bg-gray-200 dark:bg-slate-800 p-4 rounded-2xl">
+                <h3 className="text-xl font-semibold mb-3">Your plan:</h3>
+                <p className="text-base mb-px">Expiration: {"TBA"}</p>
+                <p className="text-base mb-3">Status: {"Active"}</p>
+                <p>Insert the data usage bar thingy here</p>
+            </div>
         </div>
     );
 };
