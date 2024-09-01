4c0196a35055963a40093e0ac7541fdb79f17e22
nulluser0 <diuyueng@gmail.com>
2024-09-01 22:10:50 +1000

    refactor: Update user dashboard layout and add plan information

--- client/src/pages/user/dashboard.tsx
+++ client/src/pages/user/dashboard.tsx
@@ -14,13 +14,13 @@ const Dashboard: React.FC = () => {
 
     return (
         <div className="flex flex-col items-center min-h-screen pt-7">
-            <span className="w-full text-left mb-6">
+            <span className="w-full mb-6 text-left">
                 <h1 className="text-4xl font-semibold">{greeting}.</h1>
             </span>
-            <div className="w-full mb-6 bg-gray-200 dark:bg-slate-800 p-4 rounded-2xl">
-                <h3 className="text-xl font-semibold mb-3">Your plan:</h3>
-                <p className="text-base mb-px">Expiration: {"TBA"}</p>
-                <p className="text-base mb-3">Status: {"Active"}</p>
+            <div className="w-full p-4 mb-6 bg-gray-200 dark:bg-gray-800 rounded-2xl">
+                <h3 className="mb-3 text-xl font-semibold">Your plan:</h3>
+                <p className="mb-px text-base">Expiration: {"TBA"}</p>
+                <p className="mb-3 text-base">Status: {"Active"}</p>
                 <p>Insert the data usage bar thingy here</p>
             </div>
         </div>
