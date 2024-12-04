344f85400b12232b01a2e4253562e5d5170b7935
Dao <151502370+on9au@users.noreply.github.com>
2024-12-04 00:21:29 +0000

    enhance MySQL container management script

--- mysql_docker.sh
+++ mysql_docker.sh
@@ -1,7 +1,38 @@
 #! /usr/bin/bash
 
-sudo docker run --name hiddn-website-mysql-container \
-    -e MYSQL_ROOT_PASSWORD=root \
-    -e MYSQL_DATABASE=devdb \
-    -p 3306:3306 \
-    -d mysql:latest
+CONTAINER_NAME="hiddn-website-mysql-container"
+
+start_container() {
+    sudo docker run --name $CONTAINER_NAME \
+        -e MYSQL_ROOT_PASSWORD=root \
+        -e MYSQL_DATABASE=devdb \
+        -p 3306:3306 \
+        -d mysql:latest
+    echo "MySQL container started."
+}
+
+stop_container() {
+    sudo docker stop $CONTAINER_NAME
+    echo "MySQL container stopped."
+}
+
+remove_container() {
+    sudo docker rm $CONTAINER_NAME
+    echo "MySQL container removed."
+}
+
+case "$1" in
+    start)
+        start_container
+        ;;
+    stop)
+        stop_container
+        ;;
+    remove)
+        remove_container
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|remove}"
+        exit 1
+        ;;
+esac
