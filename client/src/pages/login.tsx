ec32b4e6a13fa6a990d1ace1eca70136b6948c83
nulluser0 <diuyueng@gmail.com>
2024-12-05 22:40:17 +1100

    Add reCAPTCHA site key to environment config and update package-lock for new dependencies

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -11,7 +11,7 @@ import TextInput from '../components/logintextinput';
 const Login: React.FC = () => {
     // const ICON = '../assets/hiddn_icon.svg'; // Update with your correct path to the icon
 
-    useEffect(() => { document.title = 'Login - HiddN'; } );
+    useEffect(() => { document.title = 'Login - HiddN'; });
 
     const navigate = useNavigate();
 
