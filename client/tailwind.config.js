681e1873aa803f0e45e5c097419341693106af46
on9au <151502370+on9au@users.noreply.github.com>
2024-12-19 01:11:21 +1100

    Add Tailwind CSS typography plugin to configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
@@ -28,7 +30,7 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    typography,
   ],
 }
 
