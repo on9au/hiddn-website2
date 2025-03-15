88014653b3ca165655c5ba3b652d012fa1635b23
on9au <151502370+on9au@users.noreply.github.com>
2025-03-15 23:13:27 +1100

    Update support page to provide contact information

--- client/src/pages/user/support.tsx
+++ client/src/pages/user/support.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 
 const Support: React.FC = () => {
-    useEffect(() => { document.title = 'Support - HiddN'; } );
+    useEffect(() => { document.title = 'Support - HiddN'; });
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
-            <h1 className="text-4xl">Support</h1>
+            <h1 className="text-4xl">For the time being, please contact <a href="mailto:contact@hiddnvpn.net">contact@hiddnvpn.net</a></h1>
         </div>
     );
 };
