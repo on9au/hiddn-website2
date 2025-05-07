f4d197ea3e75cb0983218a41eefd927ca4999886
on9au <151502370+on9au@users.noreply.github.com>
2025-05-07 23:56:50 +1000

    feat: Add LICENSE file and update copyright information in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2025 OpenA0 Pty Ltd. All rights reserved.
+// This project is part of the HiddN VPN service/brand.
+// Proprietary and confidential.
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Written by Dylan Jong, 2025.
+
 pub mod config;
 pub mod db;
 pub mod errors;
