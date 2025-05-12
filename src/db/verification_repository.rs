55d6bd1cd65d895273ec52d54206242966acf547
on9au <151502370+on9au@users.noreply.github.com>
2025-05-12 23:19:21 +1000

    feat: enhance authentication flow with email verification and password strength checks

--- src/db/verification_repository.rs
+++ src/db/verification_repository.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use sqlx::MySqlPool;
 
 pub struct VerificationRepository {
@@ -8,5 +9,140 @@ impl VerificationRepository {
     /// Creates a new `VerificationRepository` instance.
     pub fn new(pool: MySqlPool) -> Self {
         Self { pool }
+    }
+
+    /// Creates a random verification code for the given email.
+    ///
+    /// This function does not insert the code into the database.
+    ///
+    /// The verification code is a 6-digit number, and is unique among currently active codes for the given email.
+    pub async fn create_random_verification_code(&self, email: &str) -> sqlx::Result<String> {
+        let verification_code = loop {
+            // Generate a random verification code
+            let code: String = (0..6)
+                .map(|_| rand::random::<u8>() % 10 + b'0')
+                .map(char::from)
+                .collect();
+
+            // Check if the code is unique for the given email
+            let is_unique = sqlx::query!(
+                r#"
+                SELECT COUNT(*) as count
+                FROM verification_codes
+                WHERE email = ? AND code = ? AND expires_at > NOW() AND is_used = 0
+                "#,
+                email,
+                code
+            )
+            .fetch_one(&self.pool)
+            .await?
+            .count
+                == 0;
+
+            if is_unique {
+                break code;
+            }
+        };
+
+        Ok(verification_code)
+    }
+
+    /// Inserts a new verification code into the database.
+    /// Ensures that the generated verification code is unique among currently active codes for the given email.
+    ///
+    /// Returns the verification code and the ID of the inserted verification code.
+    pub async fn create_verification_code(&self, email: &str) -> sqlx::Result<(String, u64)> {
+        let verification_code = self.create_random_verification_code(email).await?;
+
+        // Insert the unique verification code into the database
+        let id = self
+            .insert_verification_code(email, &verification_code)
+            .await?;
+
+        Ok((verification_code, id))
+    }
+
+    /// Inserts a new verification code into the database.
+    /// Returns the ID of the inserted verification code.
+    pub async fn insert_verification_code(
+        &self,
+        email: &str,
+        verification_code: &str,
+    ) -> sqlx::Result<u64> {
+        let result = sqlx::query!(
+            r#"
+            INSERT INTO verification_codes (email, code)
+            VALUES (?, ?)
+            "#,
+            email,
+            verification_code
+        )
+        .execute(&self.pool)
+        .await?;
+
+        Ok(result.last_insert_id())
+    }
+
+    /// Checks if the verification code is valid for the given email.
+    /// Returns true if the code is valid, false otherwise.
+    ///
+    /// Will also check if the code is expired, or if the code is used.
+    ///
+    /// This function will also mark the code as used if it is valid.
+    pub async fn is_verification_code_valid(
+        &self,
+        email: &str,
+        verification_code: &str,
+    ) -> sqlx::Result<bool> {
+        let result = sqlx::query!(
+            r#"
+            SELECT id
+            FROM verification_codes
+            WHERE email = ? AND code = ? AND expires_at > NOW() AND is_used = 0
+            "#,
+            email,
+            verification_code
+        )
+        .fetch_optional(&self.pool)
+        .await?;
+
+        if let Some(id) = result {
+            // Mark the code as used
+            sqlx::query!(
+                r#"
+                UPDATE verification_codes
+                SET is_used = 1
+                WHERE id = ?
+                "#,
+                id.id
+            )
+            .execute(&self.pool)
+            .await?;
+
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+
+    /// Returns the timestamp of the last verification code sent to the given email.
+    pub async fn get_last_verification_code_timestamp(
+        &self,
+        email: &str,
+    ) -> sqlx::Result<Option<DateTime<Utc>>> {
+        let result = sqlx::query!(
+            r#"
+            SELECT created_at
+            FROM verification_codes
+            WHERE email = ?
+            ORDER BY created_at DESC
+            LIMIT 1
+            "#,
+            email
+        )
+        .fetch_optional(&self.pool)
+        .await?;
+
+        Ok(result.map(|r| r.created_at))
     }
 }
--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -1,12 +1,23 @@
 use crate::{
+    config::GLOBAL_CONFIG,
     errors::AppResult,
-    payloads::{LoginPayload, LoginResponse, RegisterPayload},
+    payloads::{
+        ForgotPasswordPayload, LoginPayload, LoginResponse, PasswordFeedback, RegisterPayload,
+        RequestCodePayload, VerifyEmailPayload,
+    },
+    security,
     sessions::AuthSession,
     state::AppState,
 };
+use anyhow::Context;
 use axum::{Extension, Json, response::IntoResponse};
+use chrono::Utc;
+use lettre::{
+    Message, SmtpTransport, Transport, message::header::ContentType,
+    transport::smtp::authentication::Credentials,
+};
 use reqwest::StatusCode;
-use tracing::error;
+use tracing::{debug, error};
 
 /// POST `/api/auth/login`
 pub async fn login(
@@ -57,23 +68,197 @@ pub async fn register_user(
         return Ok(StatusCode::CONFLICT.into_response());
     }
 
-    Ok(Json("stub: register_user").into_response())
+    // Check if password is strong enough via zxcvbn
+    let (score, warning, suggestions) =
+        security::check_password_strength(&payload.password, &[&payload.email]);
+
+    if score < zxcvbn::Score::Three {
+        return Ok((
+            StatusCode::CONFLICT,
+            Json(PasswordFeedback {
+                warning,
+                suggestions,
+            }),
+        )
+            .into_response());
+    }
+
+    // Check if both passwords match
+    if payload.password != payload.confirm_password {
+        return Ok((
+            StatusCode::CONFLICT,
+            Json(PasswordFeedback {
+                warning: "Passwords do not match".to_string().into(),
+                suggestions: vec![],
+            }),
+        )
+            .into_response());
+    }
+
+    // Check if email verification code is valid
+    app_state
+        .verification_repository()
+        .is_verification_code_valid(&payload.email, &payload.email_verification_code)
+        .await?;
+
+    // All checks passed, create the user
+
+    // Hash the password
+    let password_hash = security::hash_password(&payload.password).await?;
+
+    app_state
+        .user_repository()
+        .create_user(&payload.email, &password_hash, false)
+        .await?;
+
+    Ok(StatusCode::OK.into_response())
 }
 
 /// POST `/api/auth/forgot-password`
-pub async fn forgot_password(/* params */) -> AppResult<impl IntoResponse> {
-    // UserRepository::forgot_password(...)
-    Ok(Json("stub: forgot_password"))
+pub async fn forgot_password(
+    Extension(app_state): Extension<AppState>,
+    Json(payload): Json<ForgotPasswordPayload>,
+) -> AppResult<impl IntoResponse> {
+    // Check if the user does not exist
+    if !app_state
+        .user_repository()
+        .user_exists(&payload.email)
+        .await?
+    {
+        return Ok(StatusCode::NOT_FOUND.into_response());
+    }
+
+    // Check if password is strong enough via zxcvbn
+    let (score, warning, suggestions) =
+        security::check_password_strength(&payload.password, &[&payload.email]);
+
+    if score < zxcvbn::Score::Three {
+        return Ok((
+            StatusCode::CONFLICT,
+            Json(PasswordFeedback {
+                warning,
+                suggestions,
+            }),
+        )
+            .into_response());
+    }
+
+    // Check if both passwords match
+    if payload.password != payload.confirm_password {
+        return Ok((
+            StatusCode::CONFLICT,
+            Json(PasswordFeedback {
+                warning: "Passwords do not match".to_string().into(),
+                suggestions: vec![],
+            }),
+        )
+            .into_response());
+    }
+
+    // Check if email verification code is valid
+    app_state
+        .verification_repository()
+        .is_verification_code_valid(&payload.email, &payload.email_verification_code)
+        .await?;
+
+    // All checks passed, change the user's password
+
+    // Hash the password
+    let password_hash = security::hash_password(&payload.password).await?;
+    app_state
+        .user_repository()
+        .update_password_by_email(&payload.email, &password_hash)
+        .await?;
+
+    Ok(StatusCode::OK.into_response())
 }
 
 /// POST `/api/auth/email/request-code`
-pub async fn request_code(/* params */) -> AppResult<impl IntoResponse> {
-    // UserRepository::request_code(...)
-    Ok(Json("stub: request_code"))
+pub async fn request_code(
+    Extension(app_state): Extension<AppState>,
+    Json(payload): Json<RequestCodePayload>,
+) -> AppResult<impl IntoResponse> {
+    // Ensure that at least 30 seconds have passed since the last request
+    let last_request = app_state
+        .verification_repository()
+        .get_last_verification_code_timestamp(&payload.email)
+        .await?;
+
+    // Ensure that at least 30 seconds have passed since the last request
+    if let Some(timestamp) = last_request {
+        let now = Utc::now();
+        let duration = now - timestamp;
+        if duration.num_seconds() < 30 {
+            return Ok(StatusCode::TOO_MANY_REQUESTS.into_response());
+        }
+    }
+
+    // Create the verification code
+    let email_verification_code = app_state
+        .verification_repository()
+        .create_random_verification_code(&payload.email)
+        .await?;
+
+    // Create tera context
+    let mut context = tera::Context::new();
+    context.insert("code", &email_verification_code);
+
+    // Render the email template
+    let email_body = app_state
+        .tera()
+        .render("verification_email.html", &context)
+        .context("Failed to render email template")?;
+
+    // Create the email
+    let email = Message::builder()
+        .from(GLOBAL_CONFIG.from_email.parse().unwrap())
+        .to(payload.email.parse().unwrap())
+        .subject(GLOBAL_CONFIG.default_subject.clone())
+        .header(ContentType::TEXT_HTML)
+        .body(email_body)
+        .context("Failed to create email")?;
+
+    let creds = Credentials::new(
+        GLOBAL_CONFIG.smtp_username.clone(),
+        GLOBAL_CONFIG.smtp_password.clone(),
+    );
+
+    debug!("creating mailer");
+
+    // Mail
+    let mailer = SmtpTransport::relay(&GLOBAL_CONFIG.smtp_server)
+        .context("Failed to create SMTP transport")?
+        .credentials(creds)
+        .build();
+
+    debug!("Sending email to {}", payload.email);
+
+    // Send the email.
+    mailer.send(&email).context("Failed to send email")?;
+
+    // Success, insert the code into the db
+    app_state
+        .verification_repository()
+        .insert_verification_code(&payload.email, &email_verification_code)
+        .await?;
+
+    Ok(StatusCode::OK.into_response())
 }
 
 /// POST `/api/auth/email/verify`
-pub async fn verify_email(/* params */) -> AppResult<impl IntoResponse> {
-    // UserRepository::verify_email(...)
-    Ok(Json("stub: verify_email"))
+pub async fn verify_email(
+    Extension(app_state): Extension<AppState>,
+    Json(payload): Json<VerifyEmailPayload>,
+) -> AppResult<impl IntoResponse> {
+    // Just check if the code is valid
+    let is_valid = app_state
+        .verification_repository()
+        .is_verification_code_valid(&payload.email, &payload.email_verification_code)
+        .await?;
+
+    if !is_valid {
+        return Ok(StatusCode::FORBIDDEN.into_response());
+    }
+
+    Ok(StatusCode::OK.into_response())
 }
